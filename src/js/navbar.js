ee55f91f442b83b921a65443a7db81018a406802
Janice <janicelin852@gmail.com>
2022-07-24 20:59:42 +0800

    justify-content-end navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -5,7 +5,7 @@ navbar =
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">\
                         <span class="navbar-toggler-icon"></span>\
                     </button>\
-                <div class="collapse navbar-collapse" id="navbar">\
+                <div class="collapse navbar-collapse justify-content-end"  id="navbar">\
                     <div class="navbar-nav">\
                         <a class="nav-link" href="https://janicelin852.github.io/JaniceLin" id="menu_work">Work</a>\
                         <a class="nav-link" href="https://janicelin852.github.io/JaniceLin/src/page/about.html" id="menu_about">About</a>\
