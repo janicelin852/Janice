817c2db8276bab0906f2f25ebce4e9bd08b6c674
Janice <janicelin852@gmail.com>
2022-07-27 18:51:42 +0800

    use js to control prev and next project

--- src/js/main.js
+++ src/js/main.js
@@ -28,10 +28,61 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+// prev next button
+var list = [
+  {
+      title:'EMS | Janice Lin',
+      src:'https://janicelin852.github.io/JaniceLin/src/page/EMS.html',
+      btn:'The Energy Management System(EMS)'
+  },
+  {
+      title:'Remote IO App | Janice Lin',
+      src:'https://janicelin852.github.io/JaniceLin/src/page/RemoteIO.html',
+      btn:'Remote IO App'
+  },
+  {
+      title:'Smart Tunnel Light | Janice Lin',
+      src:'https://janicelin852.github.io/JaniceLin/src/page/SmartTunnelLight.html',
+      btn:'Smart Tunnel Light Experience'
+  },
+
+];
+
+
+var title_name = document.title;// 抓這頁的title
+var page_index = 0;
+list.forEach((item,index) => {
+  if(item.title == title_name){
+      page_index = index;
+  }
+})
+
+// 如果不是第一頁
+if(page_index != 0){
+  document.getElementById('prev').href = list[page_index - 1].src;
+  document.getElementById('prev').text = list[page_index - 1].btn;
+
+// 如果是第一頁 前一頁按鈕消失
+}else{
+  document.getElementById('prev').style['display'] = 'none';
+}
+
+// 如果不是最後一頁
+if(page_index != list.length - 1){
+  document.getElementById('next').href = list[page_index + 1].src;
+  document.getElementById('next').text = list[page_index + 1].btn;
+
+// 如果是最後一頁 下一頁按鈕消失
+}else{
+  document.getElementById('next').style['display'] = 'none';
 }
