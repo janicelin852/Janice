c97840c2ed58b3c654460f33314c51ad0b97412b
Janice <janicelin852@gmail.com>
2022-08-23 00:28:39 +0800

    try to fix siteicon

--- src/js/siteicon.js
+++ src/js/siteicon.js
@@ -1,5 +1,5 @@
 siteicon=        
-            '<link rel="shortcut icon" href="https://janicelin852.github.io/JaniceLin/asset/img/icon.ico">';
+            '<link rel="shortcut icon" href="https://janicelin852.github.io/JaniceLin/asset/img/icon.ico" type="image/x-icon">';
             // '<link rel="shortcut icon" href="asset/img/icon.ico">';
 
 document.write(siteicon); 
