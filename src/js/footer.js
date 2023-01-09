e4423d340b906eb8a110001cda582f98beded79b
Janice <janicelin852@gmail.com>
2023-01-09 20:09:14 +0800

    remove personal info

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,9 +3,6 @@ footer=
                 <div class="container d-flex justify-content-center align-items-center py-5">\
                     <ul class="menu">\
                         <li>\
-                            <a href="mailto:janicelin852@gmail.com" target="_blank" data-lang="footer/email"></a>\
-                        </li>\
-                        <li>\
                             <a href="https://www.linkedin.com/in/janicelin852/" target="_blank" data-lang="footer/linkedIn"></a>\
                         </li>\
                         <li>\
